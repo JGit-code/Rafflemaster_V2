74512df1250b83aebc3575584b3cc5efee4b572a
Tian <tianryno01@gmail.com>
2025-04-29 21:17:20 +0200

    link fix
    link fix

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,18 +1,18 @@
 <header class="site-header">
     <div class="logo">
-        <a href="/rafflemaster1/index.php">
-            <img src="/rafflemaster1/assets/img/RaffleLogo.png" alt="Raffle Master Logo">
-            <a href="/rafflemaster1/index.php"><span>Raffle Master</span></a>
+        <a href="/index.php">
+            <img src="/assets/img/RaffleLogo.png" alt="Raffle Master Logo">
+            <a href="/index.php"><span>Raffle Master</span></a>
         </a>
     </div>
     <!-- Desktop Navigation -->
     <nav class="desktop-nav">
         <ul>
-            <li><a href="/rafflemaster1/index.php" class="nav-link">HOME</a></li>
-            <li><a href="/rafflemaster1/views/raffles.php" class="nav-link">COMPETITIONS</a></li>
-            <li><a href="/rafflemaster1/views/winners.php" class="nav-link">WINNERS</a></li>
-            <li><a href="/rafflemaster1/views/how-it-works.php" class="nav-link">HOW IT WORKS</a></li>
-            <li><a href="/rafflemaster1/views/cart.php" class="nav-link cart-link">Cart <span class="cart-count">0</span></a></li>
+            <li><a href="/index.php" class="nav-link">HOME</a></li>
+            <li><a href="/views/raffles.php" class="nav-link">COMPETITIONS</a></li>
+            <li><a href="/views/winners.php" class="nav-link">WINNERS</a></li>
+            <li><a href="/views/how-it-works.php" class="nav-link">HOW IT WORKS</a></li>
+            <li><a href="/views/cart.php" class="nav-link cart-link">Cart <span class="cart-count">0</span></a></li>
         </ul>
     </nav>
 
@@ -28,16 +28,16 @@
         <div class="mobile-nav-header">
             <div class="logo">
                 <img src="/assets/img/RaffleLogo.png" alt="Raffle Master Logo">
-                <a href="/rafflemaster1/index.php"><span>Raffle Master</span></a>
+                <a href="/index.php"><span>Raffle Master</span></a>
             </div>
             <button class="close-menu" aria-label="Close Menu">&times;</button>
         </div>
         <ul>
-            <li><a href="/rafflemaster1/index.php" class="nav-link">HOME</a></li>
-            <li><a href="/rafflemaster1/views/raffles.php" class="nav-link">COMPETITIONS</a></li>
-            <li><a href="/rafflemaster1/views/winners.php" class="nav-link">WINNERS</a></li>
-            <li><a href="/rafflemaster1/views/how-it-works.php" class="nav-link">HOW IT WORKS</a></li>
-            <li><a href="/rafflemaster1/views/cart.php" class="nav-link cart-link">Cart <span class="cart-count">0</span></a></li>
+            <li><a href="/index.php" class="nav-link">HOME</a></li>
+            <li><a href="/views/raffles.php" class="nav-link">COMPETITIONS</a></li>
+            <li><a href="/views/winners.php" class="nav-link">WINNERS</a></li>
+            <li><a href="/views/how-it-works.php" class="nav-link">HOW IT WORKS</a></li>
+            <li><a href="/views/cart.php" class="nav-link cart-link">Cart <span class="cart-count">0</span></a></li>
         </ul>
     </nav>
 </header>
